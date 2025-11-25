7fe416378318bb2b26ffecef100822742a7cab2c
Courtney Kimbrough <courtneyekimbrough@gmail.com>
2025-11-24 21:37:31 -0600

    implement feature tests

--- spec/features/sample_spec.rb
+++ spec/features/sample_spec.rb
@@ -1,5 +1,23 @@
-describe 'This project' do
-  it 'has no tests' do
-    expect(1).to eq(1)
+require 'spec_helper'
+require 'capybara/rspec'
+
+describe 'Shelf Chef App', type: :feature do
+  it 'shows the homepage' do
+    visit '/'
+    expect(page).to have_content('Whats in your kitchen?')
+    expect(page).to have_selector('form')
+  end
+
+  it 'redirects to homepage if no ingredients are entered' do
+    visit '/recipies'
+    expect(page).to have_current_path('/')
+  end
+
+  it 'shows a recipe when ingredients are entered', :vcr do
+    visit '/'
+    fill_in 'ingredients', with: 'eggs, cheese, spinach'
+    click_button 'Find Recipie'
+    expect(page).to have_content('Ingredients')
+    expect(page).to have_content('Instructions')
   end
 end
